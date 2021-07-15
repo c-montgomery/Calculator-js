91ade0d1a90ff437228f1771ec27374b2b7dd52c
c-montgomery <craym87@gmail.com>
2021-07-14 17:51:09 -0700

    completed basic addition funcitionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
+/* Add a function that puts buttonpressed attributes to false except the one being pressed. Add a condition
+if they aren't pressed, and if they are pressed, numbers should be added to the otherNumber array. If there are items in both the 
+first number, an operator being pressed is true and there's a second number, the solved equation integer should be displayed, and then
+moved to the firstNumber array  ---can't think of a reason it needs to be split, so adding the complete number to array[0] should be fine  
+---consider conolidating the  buttonPressed and operatorPressed functions\
+---Do the math functions need to be discreet or can they be added to the solve function?
+---Do I need individual operator booleans? or can there be a universal operator isPressed boolean?
+---complete solvedReset function, which adds the solution to an equation to the firstNumber variable, clears the operator and otherNumber*/
+
+
+
 //create digit buttons
 let one = document.querySelector('.one');
 let two = document.querySelector('.two');
@@ -44,6 +55,7 @@ let addButtonPressed = false;
 let subtractButtonPressed = false;
 let multiplyButtonPressed = false;
 let divideButtonPressed = false;
+let operatorIsPressed = false
 
 //create equation variables object
 let displayString = {
@@ -53,64 +65,94 @@ let displayString = {
 }
 
 //handles button presses, appends to displayString[firstNumber]
-function buttonPress(e){
-    console.log(e)
-    displayString['firstNumber'].push(e.target.innerText);
-    console.log(displayString['firstNumber']);
-    updateDisplay('number');
-}
-
-//updates display
-function updateDisplay(value) {
-    if(value === 'number'){
-        if (displayString.firstNumber.length < 9){
-        display.textContent = displayString['firstNumber'].join('');
-        }
-    }else if (value === 'symbol'){
-        display.textContent = displayString.operator
+function buttonPress(e) {
+    if (!operatorIsPressed) {
+        displayString['firstNumber'].push(e.target.innerText);
+        console.log(displayString['firstNumber']);
+        updateDisplay('number');
+    } else {
+        console.log('statemetn else\'d')
+        displayString['otherNumber'].push(e.target.innerText);
+        updateDisplay('number')
     }
 }
 
+//function for handling operator presses
+function operatorPress(e) {
+    operatorIsPressed = true;
+    if (e.target.innerText !== '=') {
+        console.log('in operator press')
+        console.log(e)
+        displayString['operator'] = e.target.innerText
+        console.log(displayString['operator'])
+        updateDisplay('symbol');
+    } else {
+        display.textContent = solve();
+    }
+}
+
+//function updates display depending on type of button press
+function updateDisplay(value){
+    console.log('in updateDisplay')
+    console.log(value)
+    if (value == 'number' && operatorIsPressed == false){
+        console.log('in first if')
+        if (displayString.firstNumber.length < 9) {
+            display.textContent = displayString['firstNumber'].join('');
+        }
+    } else if (value == 'number' && operatorIsPressed === true){
+        
+        console.log('operator pressed and update entered')
+        display.textContent = displayString['otherNumber'].join('');
+    } else if (value == 'symbol'){
+        console.log('in symbol')
+        display.textContent = displayString['operator']
+    }else{
+        console.log('no values matched')
+    }  
+}
+
+
 //checks size of otherNumber, and performs appropriate operation
-function solve(){
-    if (displayString.otherNumber.length != 0){
-        switch(displayString.operator){
+function solve() {
+    if (displayString.otherNumber.length != 0) {
+        switch (displayString.operator) {
             case '+':
-                addNumbers(displayString.firstNumber.join(''), displayString.otherNumber.join(''))
+                console.log('dis')
+                return addNumbers(parseInt(displayString.firstNumber.join('')), parseInt(displayString.otherNumber.join('')));
+                
                 break;
             case '-':
-                subtractNumbers(displayString.firstNumber.join(''), displayString.otherNumber.join(''))
+                subtractNumbers(parseInt(displayString.firstNumber.join('')), parseInt(displayString.otherNumber.join('')))
+
                 break;
-            case '/':
+            case '\00F7':
                 divideNumbers(displayString.firstNumber.join(''), displayString.otherNumber.join(''))
                 break;
             case '*':
                 multiplyNumbers(displayString.firstNumber.join(''), displayString.otherNumber.join(''))
-                break;    
+                break;
         }
-        
-        console.log(parseInt(displayString.firstNumber.join('')) + displayString.operator + parseInt(displayString.otherNumber.join('')) );
-        return parseInt(displayString.firstNumber.join('')) + displayString.operator + parseInt(displayString.otherNumber.join('')); 
+
     }
 }
 
-function addNumbers(first, second){
-    console.log()
-    return parseInt(first)+parseInt(second);
+function addNumbers(first, second) {
+    let total = (parseInt(first)+parseInt(second));
+    console.log('vvvvvvv')
+    console.log(typeof total)
+    return total;
 }
-function subtractNumbers(first, second){
-    return parseInt(first)-parseInt(second);
+function subtractNumbers(first, second) {
+    return first -second;
 }
-function divideNumbers(first, second){
-    return parseInt(first)/parseInt(second);
+function divideNumbers(first, second) {
+    return parseInt(first) / parseInt(second);
 }
-function multiplyNumbers(first, second){
-    return parseInt(first)*parseInt(second);
+function multiplyNumbers(first, second) {
+    return parseInt(first) * parseInt(second);
 }
-//function for handling operator presses
-function operatorPress(e){
-    console.log(e)
-    displayString['operator'] = e.target.innerText
-    console.log(displayString['operator'])
-    updateDisplay('symbol');
+
+function solvedReset(){
+
 }
